4549226426441aaf29ed3cabf90ecaee44f44e23
BlueBlur997 <soccer4daniels@gmail.com>
2023-06-13 19:38:14 -0700

    Step 4 complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,30 @@ import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  
-  
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup);
+
+  <div>
+  {featPupId && (
+      <div>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+      </div>
+    )}
+</div>
+
   console.log(puppies);
   return (
-    <div> {
+    <div className='App'> {
             puppies.map((puppy) => {
-              return <p key={puppy.id}>{puppy.name}</p>;
+              return <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
             })
           } 
+          {featPupId && <p>{featPupId}</p>}
     </div>
   )
 }
